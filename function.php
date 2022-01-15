6ccb6735970110360c1191d6953b855facdd33d9
MamiWebDev <78879429+MamiWebDev@users.noreply.github.com>
2022-01-16 00:50:09 +0300

    Syntax hataları düzeltildi.

--- function.php
+++ function.php
@@ -5,43 +5,24 @@ namespace auth;
 class mailAuthenticator {
 
 	public function mailauth($mail){
-
-		$exploded 			 = explode('@', $mail);
-
-		$address 			 = $exploded[1];
-
-		$website 			 = @file_get_contents("http://".$address);
-
+		$exploded = explode('@', $mail);
+		$address = $exploded[1];
+		$website = @file_get_contents("http://".$address);
 		$siteaddressexploded = explode(".", $address);
-
-		$sitename 			 = $siteaddressexploded[0];
-
-		$websitecontrol 	 = strpos($website, "mail")    ? true : false;
-
-		$websitecontroltwo 	 = strpos($website, $sitename) ? true : false;
-
+		$sitename = $siteaddressexploded[0];
+		$websitecontrol = strpos($website, "mail") ? true : false;
+		$websitecontroltwo = strpos($website, $sitename) ? true : false;
 		if(empty($website) || !isset($website) && empty($website)):
-
 			return false;
-
 		else:
-
-			if ($websitecontrol == true):
-
+			if ($websitecontrol):
 				return true;
-
-			elseif($websitecontrol == false):
-
-				if ($websitecontroltwo == true):
-					
+			else:
+				if ($websitecontroltwo):
 					return true;
-
 				endif;
-
 			endif;
-
 		endif;
-
 	}
 
 }
